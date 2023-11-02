73788593e42368206352616ded1e7328f72a5f72
Leo Gancedo <gancedoleonel@gmail.com>
2023-11-02 18:10:58 -0300

    Clase 3

--- game.js
+++ game.js
@@ -4,12 +4,24 @@ const game = canvas.getContext('2d')
 window.addEventListener('load', startGame)
 
 function startGame() {
-    // game.fillRect(0,0,100,100)
-    // game.clearRect(50,50,100,100)
-    // game.clearRect(0,0,50,50)
+    let canvasSize;
 
-    game.font = '25px Verdana'
-    game.fillStyle = 'purple'
-    game.textAlign = 'right'
-    game.fillText('Platzi', 25,25)
+    if (window.innerHeight > window.innerWidth) {
+        canvasSize = window.innerWidth * 0.8
+    } else {
+        canvasSize = window.innerHeight * 0.8
+    }
+
+    canvas.setAttribute('width', canvasSize)
+    canvas.setAttribute('height', canvasSize)
+
+    const elementsSize = (canvasSize / 11)
+
+    game.font = elementsSize + "px Verdana"
+    game.textAlign = "center"
+
+    for (let i = 1; i <= 10; i++) {
+        game.fillText(emojis["X"],elementsSize * i, elementsSize)
+    }
+
 }
