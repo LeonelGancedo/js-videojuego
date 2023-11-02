660591121ed52700a832c699616a27ae85fc0e2e
Leo Gancedo <gancedoleonel@gmail.com>
2023-11-02 18:43:02 -0300

    Clase 4

--- game.js
+++ game.js
@@ -1,22 +1,25 @@
 const canvas = document.querySelector('#game')
 const game = canvas.getContext('2d')
+let canvasSize;
+let elementsSize;
+const levelOne = maps[0]
 
-window.addEventListener('load', startGame)
+window.addEventListener('load', setCanvasSize)
+window.addEventListener('resize', setCanvasSize)
 
-function startGame() {
-    let canvasSize;
-
+function setCanvasSize () {
     if (window.innerHeight > window.innerWidth) {
         canvasSize = window.innerWidth * 0.8
     } else {
         canvasSize = window.innerHeight * 0.8
     }
-
     canvas.setAttribute('width', canvasSize)
     canvas.setAttribute('height', canvasSize)
+    elementsSize = (canvasSize / 11)
+    startGame()
+}
 
-    const elementsSize = (canvasSize / 11)
-
+function startGame() {
     game.font = elementsSize + "px Verdana"
     game.textAlign = "center"
 
