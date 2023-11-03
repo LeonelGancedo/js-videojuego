d6d548b2906eb47f4b22d69b49166cace39784e8
Leo Gancedo <gancedoleonel@gmail.com>
2023-11-03 20:17:40 -0300

    Clase 8

--- game.js
+++ game.js
@@ -1,5 +1,11 @@
 const canvas = document.querySelector('#game')
 const game = canvas.getContext('2d')
+const map = maps[0]
+const btnUp = document.querySelector('#up')
+const btnLeft = document.querySelector('#left')
+const btnRight = document.querySelector('#right')
+const btnDown = document.querySelector('#down')
+
 let canvasSize;
 let elementsSize;
 
@@ -7,28 +13,57 @@ window.addEventListener('load', setCanvasSize)
 window.addEventListener('resize', setCanvasSize)
 
 function setCanvasSize () {
-    if (window.innerHeight > window.innerWidth) {
-        canvasSize = window.innerWidth * 0.8
-    } else {
-        canvasSize = window.innerHeight * 0.8
-    }
+    window.innerHeight > window.innerWidth ? 
+    canvasSize = window.innerWidth * 0.8 : canvasSize = window.innerHeight * 0.8
     canvas.setAttribute('width', canvasSize)
     canvas.setAttribute('height', canvasSize)
     elementsSize = (canvasSize / 11)
     startGame()
 }
-
 function startGame() {
     game.font = elementsSize + "px Verdana"
     game.textAlign = "center"
-    const map = maps[0]
-    const mapRows = map.trim().split("\n")
-    const mapRowCols = mapRows.map(row => row.trim().split(""))
+    const mapRowCols = map.trim().split("\n").map(row => row.trim().split(""))
 
+    mapRowCols.forEach((row, rowIndex) => {
+        row.forEach((col, colIndex) => {
+        let emoji = emojis[col]
+        let x = elementsSize * (colIndex + 1)
+        let y = elementsSize * (rowIndex + 1)
+        game.fillText(emoji, x, y)
+        })
+    });
+}
 
-    for (let row = 1; row <= 10; row++) {
-        for (let col = 1; col <= 10; col++) {
-            game.fillText(emojis[mapRowCols[row - 1][col - 1]],elementsSize * col, elementsSize * row)
-        }
+btnUp.addEventListener('click', moveUp)
+btnDown.addEventListener('click', moveDown)
+btnLeft.addEventListener('click', moveLeft)
+btnRight.addEventListener('click', moveRight)
+window.addEventListener('keydown', moveKeys)
+
+function moveKeys (e) {
+    switch (e.key) {
+        case 'ArrowUp': moveUp()
+        break;
+        case 'ArrowDown': moveDown()
+        break;
+        case 'ArrowLeft': moveLeft()
+        break;
+        case 'ArrowRight': moveRight()
+        break;
+        default:
+        break
     }
+}
+function moveUp () {
+    console.log("Arriba");
+}
+function moveDown () {
+    console.log("Abajo");
+}
+function moveLeft () {
+    console.log("Izquierda");
+}
+function moveRight () {
+    console.log("Derecha");
 }
