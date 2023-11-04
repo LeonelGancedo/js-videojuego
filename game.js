8fbf9f1054fdfddfe0cd20ff7f5e1ac530f47650
Leo Gancedo <gancedoleonel@gmail.com>
2023-11-04 18:33:21 -0300

    Clase 10

--- game.js
+++ game.js
@@ -9,6 +9,11 @@ const btnDown = document.querySelector('#down')
 let canvasSize;
 let elementsSize;
 
+const playerPosition = {
+    x: undefined,
+    y: undefined
+}
+
 window.addEventListener('load', setCanvasSize)
 window.addEventListener('resize', setCanvasSize)
 
@@ -25,14 +30,32 @@ function startGame() {
     game.textAlign = "center"
     const mapRowCols = map.trim().split("\n").map(row => row.trim().split(""))
 
+    game.clearRect(0,0, canvasSize, canvasSize)
+
     mapRowCols.forEach((row, rowIndex) => {
         row.forEach((col, colIndex) => {
         let emoji = emojis[col]
         let x = elementsSize * (colIndex + 1)
         let y = elementsSize * (rowIndex + 1)
+
+        if(col == "O") {
+            if (!playerPosition.x && !playerPosition.y) {
+                playerPosition.x = x
+                playerPosition.y = y
+                console.log(playerPosition);
+            }
+        }
+
         game.fillText(emoji, x, y)
         })
     });
+
+    movePlayer()
+}
+
+
+function movePlayer () {
+    game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y)
 }
 
 btnUp.addEventListener('click', moveUp)
@@ -57,13 +80,30 @@ function moveKeys (e) {
 }
 function moveUp () {
     console.log("Arriba");
+    playerPosition.y -= elementsSize
+    startGame()
+    console.log(playerPosition);
+    console.log(canvasSize);
+    
 }
 function moveDown () {
     console.log("Abajo");
+    playerPosition.y += elementsSize
+    startGame()
+    console.log(playerPosition);
+    console.log(canvasSize);
 }
 function moveLeft () {
     console.log("Izquierda");
+    playerPosition.x -= elementsSize
+    startGame()
+    console.log(playerPosition);
+    console.log(canvasSize);
 }
 function moveRight () {
     console.log("Derecha");
+    playerPosition.x += elementsSize
+    startGame()
+    console.log(playerPosition);
+    console.log(canvasSize);
 }
